ae43fa270648f3bf49df0d7f4af9ab4099c22a7c
Khea Chang <kheacyc@gmail.com>
2024-07-26 11:22:05 -0700

    feat: create recruiter entry point in home page

--- recruiting-platform/src/app/page.tsx
+++ recruiting-platform/src/app/page.tsx
@@ -1,53 +1,36 @@
-import Link from "next/link";
+"use client";
 
-import { LatestPost } from "~/app/_components/post";
-import { api, HydrateClient } from "~/trpc/server";
+import { useRouter } from "next/navigation";
 
-export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
+export default function Home() {
+  const router = useRouter();
+  const handleNavigate = (path: string) => {
+    router.push(path);
+  }
 
   return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
-          </div>
+    <div className="w-full max-w-6xl mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-6">Welcome to your recruiting platform!</h1>
+      <p className="mb-7">Whether you are here to explore job opportunities or manage recruitment processes, select your role below to get started:</p>
+      <div className="flex justify-center gap-4 mb-6">
+        <button
+          className="btn w-64 rounded-full"
+          onClick={() => handleNavigate('/candidate')}
+        >
+          Candidate Dashboard
+        </button>
+        <button
+          className="btn w-64 rounded-full"
+          onClick={() => handleNavigate('/recruiter')}
+        >
+          Recruiter Dashboard
+        </button>
+      </div>
+      <div className="mt-6">
+        <p>If you have received an invitation from a recruiter to fill out an application, please click on <strong>"Candidate Dashboard"</strong> to complete the application process.</p><br />
+        <p>If you are a recruiter looking to manage job postings and applications, click on <strong>"Recruiter Dashboard"</strong> to access your tools and resources.</p>
+      </div>
 
-          <LatestPost />
-        </div>
-      </main>
-    </HydrateClient>
-  );
+    </div>
+  )
 }
---
+++ recruiting-platform/src/app/recruiter/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function RolesPage() {
+
+  return (
+    <div className="w-full max-w-6xl mx-auto p-6">
+        <h1>Roles you're recruiting for:</h1>
+    </div>
+  );
+}
